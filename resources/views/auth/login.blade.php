57b0aff15464ddb95b676d7c41cd935dc4f965c4
Ahmad Husein Hambali <ahmadhuseinh.03@gmail.com>
2023-09-28 20:03:26 +0700

    Localization pt 3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,7 +33,7 @@
                     </form>
                 </div>
                 <div class="card-footer text-center py-3">
-                    <div class="small"><a href="{{ route('register') }}">Need an account? Sign up!</a></div>
+                    <div class="small"><a href="{{ route('register') }}">Tidak ada akun? Buat baru disini</a></div>
                 </div>
             </div>
         </div>
