c4875ec8552a64e21fa176de83b58731e48aac32
aeladewi <20103064@ittelkom-pwt.ac.id>
2023-11-09 23:26:05 +0700

    Change Font Family

--- resources/views/partials/stylesheets.blade.php
+++ resources/views/partials/stylesheets.blade.php
@@ -1,4 +1,8 @@
 {{-- Default Stylesheets for all pages --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,300&display=swap" rel="stylesheet">
+
 <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}"/>
 <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}"/>
 <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}"/>
